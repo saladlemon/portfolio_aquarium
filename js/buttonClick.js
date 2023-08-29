168a6ac6dd68b5d3545cd4693fdbec7417906b5a
DoHun Jeon <107619587+saladlemon@users.noreply.github.com>
2023-08-29 20:56:07 +0900

    포트폴리오 페이지 만들었음
    첫번째

---
+++ js/buttonClick.js
@@ -0,0 +1,20 @@
+function clickhome() {
+  document.querySelector("#home").style.display = "block";
+  document.querySelector("#introduce").style.display = "none";
+  document.querySelector("#project").style.display = "none";
+}
+function clickIntro() {
+  document.getElementById("home").style.display = "none";
+  document.getElementById("introduce").style.display = "block";
+  document.getElementById("project").style.display = "none";
+}
+function clickProject() {
+  document.getElementById("home").style.display = "none";
+  document.getElementById("introduce").style.display = "none";
+  document.getElementById("project").style.display = "block";
+}
+function clickContact() {
+  document.getElementById("home").style.display = "none";
+  document.getElementById("introduce").style.display = "none";
+  document.getElementById("project").style.display = "none";
+}
